dfe51976f9a0a0144a9fb7b2d4e57a226de1dfdf
Subha22jit <arjunbauri743@gmail.com>
2022-08-25 12:48:27 +0530

    valuechange

--- sum.java
+++ sum.java
@@ -1,6 +1,6 @@
 public class sum {
     public static void main(String args[]){
-        int a = 10;
+        int a = 1;
         int b = 20;
         int sum = a + b;
         System.out.print(sum);
